9b8490f344907e823ff2e6ed53362ebe0a9bc6ed
analyticswonderful <analyticswonderful@gmail.com>
2024-08-08 10:46:23 -0400

    updated app.py

--- app copy.py
+++ app copy.py
@@ -2,14 +2,12 @@ from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-import requests
-from requests.exceptions import ConnectionError
 
-# Define the system prompt
+
 system_prompt = (
     "You are a helpful assistant. "
-    "Answer questions with concise and clear answers. "
-    "Use simple words and three sentences maximum and keep the answer concise. "
+    "Answer questions with concise and clear answerss"
+    "Use simple words and three sentences maximum and keep the answer concise "
     "Use bullet points when needed."
 )
 
@@ -21,7 +19,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# Initialize the Ollama Llama2 model
+# Initialize the Ollama Llama2 model with streaming callback
 llm = Ollama(model="gemma:2b", base_url="http://localhost:11434")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
@@ -34,11 +32,7 @@ st.write("Ask a question and get a concise, bullet-pointed answer:")
 question = st.text_input("What question do you have in mind?")
 
 if question:
-    try:
-        # Get response
-        response = chain.invoke({"question": question})
-        st.write(f"Response: {response}")
-    except ConnectionError:
-        st.error("Failed to connect to the model server. Please ensure the server is running and accessible.")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+    # Get response
+    response = chain.invoke({"question": question})
+    st.write(f"Response: {response}")
+        
